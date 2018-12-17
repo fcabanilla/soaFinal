4ba250fbf64befa09c3a4d2ea8ee65d7ee2bb284
Federico Cabanilla <federico.cabanilla@gmial.com>
2018-12-17 10:48:57 -0300

    Se corrigio el endpoint de stateHistory para obtener el listado de todos los cambios

--- controllers/stateHistory.js
+++ controllers/stateHistory.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var StateHistory = require('../models/stateHistory');
+var Device = require('../models/device');
 var jwt = require('../services/jwt');
 var mongoosePaginate = require('mongoose-pagination');
 const mongoose = require('mongoose');
@@ -13,54 +14,54 @@ function saveStateHistory(req, res){
 
 	var params = req.body;
 
-	stateHistory.device = params.device;
+	var deviceId = params.device;
 	stateHistory.typeOfData = params.typeOfData;
 	stateHistory.state = params.state;
 	stateHistory.timeStamp = Date.now();
 	// console.log({ahora: Date.now()});
 	// console.log({stateHistory: stateHistory});
 
-	stateHistory.save((err, stateHistoryStored) => {
+	Device.find({device: deviceId}).exec((err, device)=>{
 		if(err){
-			res.status(500).send({message: 'Error en el servidor'});
+			// console.log({error: err});
+			res.status(500).send({message: 'Error en la petición get device'});
 		}else{
-			if(!stateHistoryStored){
-				res.status(404).send({message: 'No se ha guardado el Historial'});
+			if(!device){
+				res.status(404).send({message: 'El dispositivo no existe.'});
 			}else{
-				res.status(200).send({stateHistory: stateHistoryStored});
+				// res.status(200).send({device});
+				stateHistory.device = device[0]._id;
+				stateHistory.save((err, stateHistoryStored) => {
+					if(err){
+
+						res.status(500).send({message: 'Error en el servidor'});
+					}else{
+						if(!stateHistoryStored){
+
+							res.status(404).send({message: 'No se ha guardado el Historial'});
+						}else{
+
+							res.status(200).send({stateHistory: stateHistoryStored});
+						}
+					}
+				});
 			}
 		}
 	});
+
 }
 
-
-function getHistory(req, res){
-	var device = req.params.id;
-	var deviceId = mongoose.Types.ObjectId(deviceId);
-
-	StateHistory.findById({"device" : deviceId}).exec((err, stateHistory)=>{
-		if(err){
-			res.status(500).send({message: 'Error en la petición', err, err});
-		}else{
-			if(!stateHistory){
-				res.status(404).send({message: 'El Historial de este dispositivo no existe.'});
-			}else{
-				res.status(200).send({stateHistory});
-			}
-		}
-	});
-}
-
-// function getAllHistory(req, res){
 //
-// 	var find = StateHistory.find({}).sort('timeStamp');
+// function getHistory(req, res){
+// 	var device = req.params.id;
+// 	var deviceId = mongoose.Types.ObjectId(deviceId);
 //
-// 	find.populate({path: 'device'}).exec((err, stateHistory) => {
+// 	StateHistory.findById({"device" : deviceId}).exec((err, stateHistory)=>{
 // 		if(err){
 // 			res.status(500).send({message: 'Error en la petición', err, err});
 // 		}else{
 // 			if(!stateHistory){
-// 				res.status(404).send({message: 'No hay historial'});
+// 				res.status(404).send({message: 'El Historial de este dispositivo no existe.'});
 // 			}else{
 // 				res.status(200).send({stateHistory});
 // 			}
@@ -68,9 +69,26 @@ function getHistory(req, res){
 // 	});
 // }
 
+function getAllHistory(req, res){
+
+	var find = StateHistory.find({}).sort('timeStamp');
+
+	find.populate({path: 'device'}).exec((err, stateHistory) => {
+		if(err){
+			res.status(500).send({message: 'Error en la petición', err, err});
+		}else{
+			if(!stateHistory){
+				res.status(404).send({message: 'No hay historial'});
+			}else{
+				res.status(200).send({stateHistory});
+			}
+		}
+	});
+}
+
 module.exports = {
 	saveStateHistory,
-	getHistory
-	// getAllHistory
+	// getHistory
+	getAllHistory
 	// getStatesHistory
 };
